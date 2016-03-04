98e634c89ee6db0d62c1f0c41bebbd092aa0e9ef
Stalullsstigen <andstig@student.chalmers.se>
2016-03-04 23:15:51 +0100

    Saves each line in own matrix

--- MATLAB/Residuework.m
+++ MATLAB/Residuework.m
@@ -21,46 +21,56 @@ pos = find(searchterm);
 search2 = [];
 for i = pos
     
-   if((strcmp({gfl.Model(1).Atom(i).resName},res_name)) == 1 && gfl.Model(1).Atom(i).resSeq == 32) 
+   if((strcmp({gfl.Model(1).Atom(i).resName},res_name)) == 1 && gfl.Model(1).Atom(i).resSeq == 31) 
        search2 = [search2 i];
    end
 end
 pos = search2;
+
 X=[gfl.Model(1).Atom(pos).X];
 Y=[gfl.Model(1).Atom(pos).Y];
-Z=[gfl.Model(1).Atom(pos).Z];
-save = zeros(88,88);
-m1 = [];
-m2 = [];
-m3 = [];
-for i = 1:(length(pos)-1)
-    for j = (i+1):(length(pos))
-       
-        ai = [X(i); Y(i); Z(i)];
-        aj = [X(j); Y(j); Z(j)];
-        
-        distance = norm(ai-aj);
-        Zdiff = abs(Z(i) - Z(j));
-        
-        
-        hold on
-        if(distance < 5.27 && Zdiff > 3.7)
-            Xs = [X(i) X(j)];
-            Ys = [Y(i) Y(j)];
-            Zs = [Z(i) Z(j)];
-            
-            saved(i,j) = 1;
-            plot3(Xs, Ys, Zs,'o');
-        
-        end
-       
-    end
-end
+Z=[gfl.Model(1).Atom(pos).Z]; 
+
 TX=transpose(X);
 TY=transpose(Y);
 TZ=transpose(Z);
 
 coords=[TX TY TZ];
+
+%% test 
+
+numberOfLists = 0;
+extraplot = [coords pos(:)];
+extraplot = sortrows(extraplot, 3);
+for x = 1:(length(extraplot))
+   xs = extraplot(x, :)
+   saved = 0;
+   for y1 = 1:numberOfLists
+       y1
+       y2 = test{y1}
+       ys = y2(size(y2, 1), :)
+       ai = [ys(1); ys(2); ys(3)]
+       aj = [xs(1); xs(2); xs(3)]
+        
+       
+       distance = norm(ai-aj);
+       Zdiff = abs(xs(3) - ys(3))
+       if(distance < 5.29)
+           if(Zdiff > 3.7)
+              test{y1} = [y2; xs]
+              saved = 1;
+           end
+       end
+   end
+   if(saved == 0)
+      numberOfLists = numberOfLists + 1
+      test{numberOfLists} = xs
+   end
+end
+
+for x = 1:numberOfLists
+    test{x}
+end
 
 %plot3(X,Y,Z, '.')
 
@@ -70,3 +80,48 @@ coords=[TX TY TZ];
 % scatter3(X,Y,Z,50, cmap)
 
 % line(X,Y,Z)
+
+hold on
+ 
+B = linspace(0,30);
+C = linspace(25,60);
+A = 0 +(-4.8241*B)+24.9822*C;
+%plot3(B,C,A)
+hold on
+
+P1 = coords(1,:)
+P2 = coords(7,:)
+P3 = coords(13,:)
+normal = cross(P1-P2,P1-P3)
+b1 = 4.8241;
+b2 = -24.9822;
+b3 = 504.3998;
+point2 = [b1 b2 b3];
+
+points = [point2;P2];
+
+
+b1 = b1/-b3
+b2 = b2/-b3
+
+Xn = linspace(0,300);
+Yn = linspace(20,600);
+Zn = b1*Xn + b2*Yn;
+hold on
+plot3(points(:,1),points(:,2), points(:,3))
+hold on
+
+syms x y z
+P= [x y z];
+planefunction = dot(normal, P-P1)
+hold on
+zplane = solve(planefunction, z);
+hold on
+ezmesh(zplane, [0,28,25,50])
+hold on
+plot(normal)
+hold on
+plot3(coords(:,1),coords(:,2), coords(:,3), 'o')
+hold on
+axis([0 30 25 55 60 90])
+hold off
